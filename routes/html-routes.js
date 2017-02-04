b5d24c8fb677bf4a56aa6bfaaf8c982eab7c9e0f
Brandon <theeternalvigil@gmail.com>
2017-02-03 22:26:37 -0800

    handlebar partials updated to newer template

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app){
 	});
 	
 	app.get("/generator", function(request, response){
-		response.render("idea",{layout: "generator.handlebars"});
+		response.render("tech",{layout: "generator.handlebars"});
 	});
 	
 	app.get("/submissions", function(request, response){
